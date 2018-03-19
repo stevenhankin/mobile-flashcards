9173dc88572eb600d99da22cadd8c352aa3a173f
Steve Hankin <steven.hankin@gmail.com>
2018-03-19 14:08:41 +0000

    refactor: simplified arrow functions
    Approach based on Dan Abramov's video

--- actions/index.js
+++ actions/index.js
@@ -4,35 +4,26 @@ export const ADD_CARD = 'ADD_CARD';
 export const LOAD_PERSISTED_STATE = 'LOAD_PERSISTED_STATE';
 
 
-export function loadPersistedState(state) {
-    return {
-        type: LOAD_PERSISTED_STATE,
-        state,
-    }
-}
+export const loadPersistedState = (state) => ({
+    type: LOAD_PERSISTED_STATE,
+    state,
+});
 
-export function addCard(deckTitle, question, answer) {
-    return {
-        type: ADD_CARD,
-        deckTitle,
-        question,
-        answer,
-    }
-}
+export const addCard = (deckTitle, question, answer) => ({
+    type: ADD_CARD,
+    deckTitle,
+    question,
+    answer,
+});
 
 
-export function addDeck(deckTitle) {
-    return {
-        type: ADD_DECK,
-        deckTitle,
-    }
-}
+export const addDeck = (deckTitle) => ({
+    type: ADD_DECK,
+    deckTitle,
+});
 
 
-export function receiveDecks(decks) {
-    return {
-        type: RECEIVE_DECKS,
-        decks,
-    }
-}
-
+export const receiveDecks = (decks) => ({
+    type: RECEIVE_DECKS,
+    decks,
+});
